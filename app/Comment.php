2a1a93f30c5afcf61a8d715ed585d0e507a06ae8
3A532059 <coco214034@gmail.com>
2018-11-21 21:44:41 +0800

    增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Post;
 
 class Comment extends Model
 {
     //
-    protected $table='comments';
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -3,14 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Comment;
 
 class Post extends Model
 {
     //
-    protected $table = 'posts';
-    protected$fillable=[
-        'title',
-        'content',
-        'is_feature'
-    ];
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
